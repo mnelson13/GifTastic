140308291c7d8f637dadf82fd680b538a31eb881
MNelson <melissa.nelson13@yahoo.com>
2018-09-30 15:57:09 -0500

    fixed bug with add more button

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 * Click on a button to see gifs for that animal.
 
-* Click on a gif to animate it
+* Click on a gif to animate it.
 
 * You can add any animal button you would like using the form!
+
+* Add additional gifs to the current animal chosen by clicking the "add more gifs" button.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -110,8 +110,23 @@ $("#addMore").on("click", function() {
             }
 
         });
+        
 
     });
+
+    $(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+
 
 })
 
